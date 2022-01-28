a3b81812593572e3146f018d658e4f8ceb0fdafe
zriyansh <prik2two@gmail.com>
2022-01-28 20:26:22 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # thread-processing
+
+Can someone make a frontend UI for this?
+
+The page should have 2 text fields 
+1. that accepts root directory (where to begin searching from)
+2. fileName to be searched for. 
+
+and 2 buttons 
+1. use single thread processing
+2. use multi thread processing
+
+when the user clicks on any of the above 2 buttons, the integrated terminal (or terminal popup) should display the searching logs and at the end, show time it required to search the file(s). 
+
+That's all. 
--- main.go
+++ main.go
@@ -1,19 +1,67 @@
+// this is an implementation of single and multi thread search algo to find files in a OS, given a root address and file name.
+
 package main
 
-import "fmt"
-
-var (
-	matches []string
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
-func fileSearch(root, filename string) {
-	fmt.Printf("going to search in ", root)
+var (
+	matches   []string
+	waitgroup = sync.WaitGroup{}
+	lock      = sync.Mutex{}
+	// we meed locking system because multiple threads may want to update the 'matches slice of string at a time, creating duplicate data'
+)
 
+func fileSearchSingleThread(root, filename string) {
+	fmt.Println("going to search in ", root)
+	files, _ := ioutil.ReadDir(root)
+	for _, file := range files {
+		if strings.Contains(file.Name(), filename) {
+			matches = append(matches, filepath.Join(root, file.Name())) // C:\tools\readme.md, adds filename to root name.
+		}
+		if file.IsDir() { // if file found is a directory, we make a recursive call
+			fileSearchSingleThread(filepath.Join(root, file.Name()), filename)
+		}
+	}
+}
+
+func fileSearchMultiThread(root, filename string) {
+	fmt.Println("going to search in ", root)
+	files, _ := ioutil.ReadDir(root)
+	for _, file := range files {
+		if strings.Contains(file.Name(), filename) {
+			lock.Lock()
+			matches = append(matches, filepath.Join(root, file.Name())) // C:\tools\readme.md, adds filename to root name.
+			lock.Unlock()
+		}
+		if file.IsDir() { // if file found is a directory, we make a recursive call
+			waitgroup.Add(1)
+			go fileSearchMultiThread(filepath.Join(root, file.Name()), filename)
+		}
+	}
+	waitgroup.Done()
 }
 
 func main() {
-	fileSearch("C:/tools", "readme.md")
+	root := "/Users/priyanshkhodiyar"
+	fileName := "pic.png"
+
+	waitgroup.Add(1)
+	go fileSearchMultiThread(root, fileName)
+	waitgroup.Wait()
+	// comment and above 3 lines and uncomment the fileSearchSingleThread() to run single thread file search and vice cersa.
+
+	// change thr root and filename and search for whatever you want,
+	// if filename = "pic.png", it will not exactly match for that, but "my_pic.png" file will also be a valid result
+
+	// fileSearchSingleThread(root, fileName)
+
 	for _, file := range matches {
-		fmt.Println("Found file", file)
+		fmt.Println("Found file ", file)
 	}
 }
